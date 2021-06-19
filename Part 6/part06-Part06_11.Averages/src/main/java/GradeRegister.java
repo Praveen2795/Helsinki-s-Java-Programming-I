725782734a6dfebf20d8b4e8fbc42186ad5cc404
Praveen Chandrasekaran <81367485+Praveen271195@users.noreply.github.com>
2021-06-19 14:35:56 +0530

    Uploaded exercise 11

---
+++ Part 6/part06-Part06_11.Averages/src/main/java/GradeRegister.java
@@ -0,0 +1,79 @@
+
+import java.util.ArrayList;
+
+public class GradeRegister {
+
+    private ArrayList<Integer> grades;
+    private ArrayList<Integer> points;
+
+    public GradeRegister() {
+        this.grades = new ArrayList<>();
+        this.points = new ArrayList<>();
+    }
+
+    public void addGradeBasedOnPoints(int points) {
+        this.grades.add(pointsToGrade(points));
+        this.points.add(points);
+    }
+
+    public int numberOfGrades(int grade) {
+        int count = 0;
+        for (int received : this.grades) {
+            if (received == grade) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int pointsToGrade(int points) {
+
+        int grade = 0;
+        if (points < 50) {
+            grade = 0;
+        } else if (points < 60) {
+            grade = 1;
+        } else if (points < 70) {
+            grade = 2;
+        } else if (points < 80) {
+            grade = 3;
+        } else if (points < 90) {
+            grade = 4;
+        } else {
+            grade = 5;
+        }
+
+        return grade;
+    }
+    
+    public double averageOfGrades(){
+        if(this.grades.isEmpty()){
+            return -1;
+        }else{
+            return sumOfGradeArrayList()/this.grades.size();
+        } 
+    }
+    
+    public double averageOfPoints(){
+        if(this.points.isEmpty()){
+            return -1;
+        }else{
+            return sumOfPointArrayList()/this.points.size();
+        } 
+    }
+    
+    public double sumOfGradeArrayList(){
+        double sum = 0;
+        for(int i: this.grades){
+            sum = sum+i;
+        } return sum;
+    }
+    
+    public double sumOfPointArrayList(){
+        double sum = 0;
+        for(int i: this.points){
+            sum = sum+i;
+        }return sum;
+    }  
+}
